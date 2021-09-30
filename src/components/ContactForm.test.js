6aeb8b5621c06ff11439b4b45b0aaaa5090e57c4
unknown <jacobillig@gmail.com>
2021-09-30 15:24:07 -0500

    wokring through problems

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import {render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    const headerEl = screen.queryByText(/contact form/i);
+    expect(headerEl).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm/>);
+    const firstNameInput = screen.getAllByLabelText("First Name*");
+    // userEvent.type(firstNameInput, "Jacob");
+    // expect(firstNameInput).toBeGreaterThanOrEqual(5);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm/>);
+    // const firstNameInput = screen.getAllByLabelText("First Name*");
+    // const lastNameInput = screen.getAllByLabelText("Last Name*");
+    // const emailInput = screen.getAllByLabelText("Email*");
+    // expect(firstNameInput).toBeGreaterThanOrEqual(1);
+    // expect(lastNameInput).toBeGreaterThanOrEqual(1);
+    // expect(emailInput).toBeGreaterThanOrEqual(1);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
